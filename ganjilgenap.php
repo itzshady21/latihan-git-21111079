272060e65ca9f9638c99b133a92258b51e8cef8e
itzshady21 <irsyadi2109@gmail.com>
2024-05-07 14:19:59 +0700

    pemindahan dan perubahan

--- ganjilgenap.php
+++ ganjilgenap.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Penentuan Plat Kendaraan</title>
+    <title>Penentuan Plat Kendaraann</title>
 </head>
 <body>
     <h1>Penentuan Plat Kendaraan</h1>
